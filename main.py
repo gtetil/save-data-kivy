0c15f9fe24967502b1b3ef54e5bebf3288fc93f3
gtetil <garrett.tetil@gmail.com>
2017-01-06 15:20:10 -0500

    pop works

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from kivy.uix.popup import Popup
 from kivy.clock import Clock
 from kivy.properties import StringProperty, NumericProperty, ObjectProperty
 from kivy.config import Config
-from kivy.storage.jsonstore import JsonStore
+import json
+from functools import partial
 
 from kivy.core.window import Window
 
@@ -22,10 +23,11 @@ Config.set('graphics', 'maxfps', '10')
 class MainScreen(BoxLayout):
     dynamic_layout = ObjectProperty(None)
     indicator_layout = ObjectProperty(None)
+    button_edit_popup = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
-        self.dynamic_layout.build_layout(self.indicator_layout)
+        self.dynamic_layout.build_layout(self.indicator_layout, self.button_edit_popup)
         self.dynamic_layout.save_layout(self.indicator_layout)
 
 
@@ -35,18 +37,33 @@ class MainApp(App):
         return MainScreen()
 
 class DynamicLayout(Widget):
-    store = JsonStore('dynamic_layout.json')
+    layout_file = 'dynamic_layout.json'
 
     def save_layout(self, layout):
+        data = {}
         for indicator in layout.children:
-            print(indicator.id)
-            self.store.put(indicator.id, label=indicator.text)
+            label = {}
+            label['label'] = indicator.text
+            data[indicator.id] = label
+        with open(self.layout_file, 'w') as fp:
+            json.dump(data, fp, sort_keys=True, indent=4)
 
-    def build_layout(self, layout):
+    def build_layout(self, layout, popup):
+        with open(self.layout_file, 'r') as fp:
+            data = json.load(fp)
         for i in range(6):
-            indicator_label = self.store.get('indicator_'+str(i))['label']
+            indicator_label = data['indicator_'+str(i)]['label']
             indicator_button = Button(text=indicator_label, id='indicator_' + str(i))
-            layout.add_widget(indicator_button, i)
+            indicator_button.bind(on_press=partial(popup.edit_popup, 'indicator_' + str(i)))
+            layout.add_widget(indicator_button)
+
+class ButtonEditPopup(Popup):
+    label_input = ObjectProperty(None)
+
+    def edit_popup(self, instance, button):
+        self.label_input.text = button.text
+        self.open()
+
 
 if __name__ == '__main__':
 
