6fbf2b5a0fb586070e2cbdde6e8b2406a85a7abe
gtetil <garrett.tetil@gmail.com>
2017-01-07 20:05:37 -0500

    completed button/toggle button code.

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.popup import Popup
 from kivy.clock import Clock
-from kivy.properties import StringProperty, NumericProperty, ObjectProperty
+from kivy.properties import StringProperty, NumericProperty, ObjectProperty, BooleanProperty
 
 import json
 from functools import partial
@@ -36,15 +37,22 @@ class MainApp(App):
 
 class DynamicLayout(Widget):
     indicator_layout = ObjectProperty(None)
+    button_layout = ObjectProperty(None)
+    indicator_edit_popup = ObjectProperty(None)
     button_edit_popup = ObjectProperty(None)
     layout_file = 'dynamic_layout.json'
 
     def save_layout(self):
         data = {}
         for indicator in self.indicator_layout.children:
-            label = {}
-            label['label'] = indicator.text
-            data[indicator.id] = label
+            config = {}
+            config['label'] = indicator.text
+            data[indicator.id] = config
+        for button in self.button_layout.children:
+            config = {}
+            config['label'] = button.text
+            config['toggle'] = button.toggle
+            data[button.id] = config
         with open(self.layout_file, 'w') as fp:
             json.dump(data, fp, sort_keys=True, indent=4)
 
@@ -54,24 +62,56 @@ class DynamicLayout(Widget):
         for i in range(6):
             indicator_label = data['indicator_'+str(i)]['label']
             indicator_button = Button(text=indicator_label, id='indicator_' + str(i))
-            indicator_button.bind(on_press=partial(self.button_edit_popup.edit_popup, 'indicator_' + str(i)))
+            indicator_button.bind(on_press=partial(self.indicator_edit_popup.edit_popup, 'indicator_' + str(i)))
             self.indicator_layout.add_widget(indicator_button)
+
+            button_label = data['button_' + str(i)]['label']
+            toggle = data['button_' + str(i)]['toggle']
+            if toggle == "True":
+                button = DynToggleButton(text=button_label, id='button_' + str(i))
+            else:
+                button = DynButton(text=button_label, id='button_' + str(i))
+            button.bind(on_press=partial(self.button_edit_popup.edit_popup, 'button_' + str(i)))
+            self.button_layout.add_widget(button)
+
+class IndicatorEditPopup(Popup):
+    ind_label_input = ObjectProperty(None)
+    indicator = ObjectProperty(None)
+    dynamic_layout = ObjectProperty(None)
+
+    def edit_popup(self, instance, indicator):
+        self.indicator = indicator
+        self.ind_label_input.text = self.indicator.text
+        self.open()
+
+    def save_indicator(self):
+        self.indicator.text = self.ind_label_input.text
+        self.dynamic_layout.save_layout()
+        self.dismiss()
 
 class ButtonEditPopup(Popup):
     label_input = ObjectProperty(None)
+    toggle_check = ObjectProperty(None)
     button = ObjectProperty(None)
     dynamic_layout = ObjectProperty(None)
 
     def edit_popup(self, instance, button):
         self.button = button
-        self.label_input.text = button.text
+        self.label_input.text = self.button.text
+        self.toggle_check.active = self.button.toggle
         self.open()
 
     def save_button(self):
         self.button.text = self.label_input.text
+        self.button.toggle = self.toggle_check.active
         self.dynamic_layout.save_layout()
         self.dismiss()
 
+class DynToggleButton(ToggleButton):
+    toggle = BooleanProperty(True)
+
+class DynButton(Button):
+    toggle = BooleanProperty(False)
 
 if __name__ == '__main__':
 
